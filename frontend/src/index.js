d2565506bbfd7b048d156483a953c3cab50a47eb
lithish <lithishvivek046@gmail.com>
2025-08-11 14:01:44 +0530

    app working in the browser

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 // frontend/src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'; // <-- new import in React 18
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container); // create a root
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
